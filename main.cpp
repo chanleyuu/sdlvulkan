e8f9be1e41895a16e66b2ee8015249d8671952e8
chanleyuu <hanley-c3@ulster.ac.uk>
2020-06-29 21:43:40 +0100

    palm tree

--- main.cpp
+++ main.cpp
@@ -36,8 +36,8 @@
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-const std::string MODEL_PATH = "models/viking_room.obj";
-const std::string TEXTURE_PATH = "textures/viking_room.png";
+const std::string MODEL_PATH = "models/palm_tree.obj";
+const std::string TEXTURE_PATH = "textures/palm_treetex.png";
 
 const int MAX_FRAMES_IN_FLIGHT = 2;
 
